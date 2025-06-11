f8736538960459119f67c4b5f8d9107cd229c71a
ErionSopi <erionsopi22@gmail.com>
2025-06-11 10:26:11 +0200

    token

---
+++ backend/src/main/java/com/example/labkursSpring/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.labkursSpring.security;
+
+import com.example.labkursSpring.model.Users;
+import com.example.labkursSpring.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Users user = userRepo.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return org.springframework.security.core.userdetails.User
+            .withUsername(user.getEmail())
+            .password(user.getPassword())
+            .authorities("USER")
+            .build();
+    }
+}
