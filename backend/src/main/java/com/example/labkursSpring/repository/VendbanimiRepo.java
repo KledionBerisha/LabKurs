556cdc96f3244c2c525fd576ef1c3f88948f25ff
ErionSopi <erionsopi22@gmail.com>
2025-10-01 00:47:56 +0200

    update

--- backend/src/main/java/com/example/labkursSpring/repository/VendbanimiRepo.java
+++ backend/src/main/java/com/example/labkursSpring/repository/VendbanimiRepo.java
@@ -2,5 +2,6 @@ package com.example.labkursSpring.repository;
 import com.example.labkursSpring.model.Vendbanimi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+
 public interface VendbanimiRepo extends JpaRepository<Vendbanimi, Long> {
 }
